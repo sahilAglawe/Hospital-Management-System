a5bc7144afcb4d41c1bcb9dcf4e2845b204974cc
SahilAglawe <sahilaglawe66@gmail.com>
2025-06-18 00:19:40 +0530

    Implement appointment management functionality with CRUD operations and update appointment schema

--- Backend/models/appointmentSchema.js
+++ Backend/models/appointmentSchema.js
@@ -59,7 +59,7 @@ const appointmentSchema = new mongoose.Schema({
     },
     hasVisited: {
         type: Boolean,
-        required: true,
+        default: false,
     },
     doctorId:{
         type: mongoose.Schema.Types.ObjectId,
